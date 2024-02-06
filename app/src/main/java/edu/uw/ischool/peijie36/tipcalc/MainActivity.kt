a7ecd5a4e716b6bf25f342fe24366fa648fa73e7
peijie36 <peijiezheng2001@gmail.com>
2024-02-06 14:28:05 -0800

    Limit to 2 decimal places for amount input

--- app/src/main/java/edu/uw/ischool/peijie36/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/peijie36/tipcalc/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.uw.ischool.peijie36.tipcalc
 
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         val amountInput = findViewById<EditText>(R.id.et_amount)
         val tipButton = findViewById<Button>(R.id.btn_tip)
+
+        amountInput.filters = arrayOf(DecimalDigitsInputFilter(2))
+
         amountInput.addTextChangedListener {
             tipButton.isEnabled = !amountInput.text.toString().isBlank()
         }
@@ -28,5 +33,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun round2DecimalPlaces(input: Double): Double {
         return String.format("%.2f", input).toDouble()
+    }
+}
+
+class DecimalDigitsInputFilter(private val digitsAfterDecimal: Int) : InputFilter {
+    override fun filter(source: CharSequence?, start: Int, end: Int, dest: Spanned?, dstart: Int, dend: Int): CharSequence? {
+        val builder = StringBuilder(dest)
+        builder.replace(dstart, dend, source?.subSequence(start, end).toString() ?: "")
+
+        val pattern = Regex("^\\d*\\.?\\d{0,$digitsAfterDecimal}\$")
+
+        if (!builder.toString().matches(pattern)) {
+            return ""
+        }
+        return null
     }
 }
